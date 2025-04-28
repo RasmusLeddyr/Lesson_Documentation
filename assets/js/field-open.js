ffe03561e6d5a87dfd02441110f7d8df4e6ee1f0
RasmusLeddyr <scud-131@hotmail.com>
2025-04-28 11:23:55 +0200

    JavaScript notes

--- assets/js/field-open.js
+++ assets/js/field-open.js
@@ -1,21 +1,29 @@
-// Dev option
+// dev option, toggle on/off the "closeAll" feature
 const closeAll = true;
 
+// fetch elements
 const fields = document.querySelectorAll("#school .field");
 const fieldButtons = document.querySelectorAll(".field-button");
-fieldButtons.forEach(function (item) {
-  item.addEventListener("click", function () {
+
+// iterate buttons
+fieldButtons.forEach((item) => {
+  // detect click
+  item.addEventListener("click", () => {
     const container = item.parentNode.parentNode;
+    // if closeAll is true
     if (closeAll == true) {
+      // detect if container is open
+      // close all containers
+      // open current container if it was closed
       const isOpen = container.classList.contains("open");
-      fields.forEach(function (field) {
-        console.log(field);
+      fields.forEach((field) => {
         field.classList.remove("open");
       });
       if (isOpen == false) {
         container.classList.toggle("open");
       }
     } else {
+      // toggle current container on/off
       container.classList.toggle("open");
     }
   });
